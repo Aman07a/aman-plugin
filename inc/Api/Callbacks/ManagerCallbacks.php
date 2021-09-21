a1715a64933639826969fe65f996ac689857990e
Aman Ahmed <aman16.aa17@gmail.com>
2021-09-21 09:36:19 +0200

    Removed SCSS Files

--- inc/Api/Callbacks/ManagerCallbacks.php
+++ inc/Api/Callbacks/ManagerCallbacks.php
@@ -25,10 +25,20 @@ class ManagerCallbacks extends BaseController
     {
         $name = $args['label_for'];
         $classes = $args['class'];
+        
         $checkbox = get_option($name);
-        echo '<div class="'. $classes . '">
+        echo '<div class="' . $classes . '">
                 <input type="checkbox" name="' . $name . '" value="1" class="' . $classes . '" ' . ($checkbox ? 'checked' : '') . '>
-                <label for="'. $name . '"><div></div></label>
+                <label for="' . $name . '"><div></div></label>
             </div>';
+
+        // $option_name = $args['option_name'];
+        // $checkbox = get_option($option_name);
+        // $checked = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
+        // echo '<div class="' . $classes . '">
+        //         <input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" 
+        //             value="1" class="" ' . ($checkbox ? 'checked' : '') . '>
+        //         <label for="' . $name . '"><div></div></label>
+        //     </div>';
     }
 }
